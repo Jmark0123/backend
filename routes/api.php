e19eb0483d24f580e75f80b71f9730b0ddbdfffb
jmark0123 <jmarkanthonylejista857@gmail.com>
2023-05-19 10:20:33 +0800

    added logout and correct groups

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CarouselItemsController;
@@ -15,23 +16,26 @@ use App\Http\Controllers\Api\UserController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+    route:: post('/login', [AuthController::class ,'login'])->name('user.login');
+    route:: post('/user',  [UserController::class ,'store'])->name('user.store');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    route:: post('/logout', [AuthController::class ,'logout']);
+
+    Route::controller(CarouselItemsController::class)->group(function () {  
+        route:: get('/carousel',          'index');
+        route:: get('/carousel/{id}',     'show');
+        route:: post('/carousel',         'store');
+        route:: put('/carousel/{id}',     'update');
+        route:: delete('/carousel/{id}',  'destroy');
+    });
+Route::controller(UserController::class)->group(function () {  
+        route:: get('/user',              'index');
+        route:: get('/user/{id}',         'show');
+        route:: put('/user/{id}',         'update')->name('user.update');
+        route:: put('/user/email/{id}',   'email')->name('user.email');
+        route:: put('/user/password/{id}','password')->name('user.password');
+        route:: delete('/user/{id}',      'destroy');
+    });
+
 });
-route:: get('/carousel',  [CarouselItemsController::class, 'index']);
-route:: get('/carousel/{id}',  [CarouselItemsController::class, 'show']);
-route:: post('/carousel',  [CarouselItemsController::class, 'store']);
-route:: put('/carousel/{id}',  [CarouselItemsController::class, 'update']);
-route:: delete('/carousel/{id}',  [CarouselItemsController::class, 'destroy']);
-
-route:: get('/user',  [UserController::class, 'index']);
-route:: get('/user/{id}',  [UserController::class, 'show']);
-route:: post('/user',  [UserController::class, 'store'])->name('user.store');
-route:: put('/user/{id}',  [UserController::class, 'update'])->name('user.update');
-route:: put('/user/email/{id}',  [UserController::class, 'email'])->name('user.email');
-route:: put('/user/password/{id}',  [UserController::class, 'password'])->name('user.password');
-route:: delete('/user/{id}',  [UserController::class, 'destroy']);
-
-
-
